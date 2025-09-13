1adec529dae22038f72a7a5578cd42a43424e4c3
Gasan A. <gasan.alkadarskiy@gmail.com>
2025-09-13 15:02:23 +0300

    Add inline buttons

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from telegram import (
     Update,
     ReplyKeyboardMarkup,
     KeyboardButton,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
 )
 from telegram.ext import (
     ApplicationBuilder,
@@ -13,6 +15,7 @@ from telegram.ext import (
     ConversationHandler,
     MessageHandler,
     filters,
+    CallbackQueryHandler,
 )
 
 load_dotenv()
@@ -20,7 +23,15 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
-FIRST_MESSAGE, GET_ANSWER, GET_NAME, GET_NUMBER, GET_EMAIL, GET_AGREEMENT = range(6)
+(
+    FIRST_MESSAGE,
+    GET_ANSWER,
+    GET_NAME,
+    GET_NUMBER,
+    GET_EMAIL,
+    GET_AGREEMENT,
+    INLINE_BUTTON,
+) = range(7)
 AGREEMENT_TEXT = (
     "Для отправки вам данных по интересующим компаниям, нам необходимо "
     "подтверждение на обработку данных. Мы не передаем их третьим лицам. "
@@ -122,17 +133,76 @@ async def get_email(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def get_agreement(update: Update, context: ContextTypes.DEFAULT_TYPE):
     message = update.effective_message.text
     context.user_data["agreement"] = message
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "Базовый",
+                callback_data="basic",
+            ),
+            InlineKeyboardButton(
+                "Расширенный",
+                callback_data="extended",
+            ),
+        ]
+    ]
+    markup = InlineKeyboardMarkup(keyboard)
     if message.strip().lower() == "согласен":
         await context.bot.send_message(
-            chat_id=update.effective_user.id, text=FEATURES_TEXT
+            chat_id=update.effective_user.id,
+            text="Выберите план",
+            reply_markup=markup,
         )
         await context.bot.send_message(
             chat_id=os.getenv("ADMIN_ID"), text=f"{context.user_data}"
         )
+        return INLINE_BUTTON
     else:
         await context.bot.send_message(
             chat_id=update.effective_user.id,
             text="К сожалению, тогда мы не можем предоставить вам функционал",
+        )
+        return FIRST_MESSAGE
+
+
+async def get_leads(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    if query.data == "basic":
+        keyboard = [
+            [
+                InlineKeyboardButton("Получить", callback_data="get"),
+                InlineKeyboardButton("Не интересует", callback_data="no"),
+            ]
+        ]
+        markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(
+            text="Получите [базовый](https://parents-chew-q3e.craft.me/2PpY6OcIw0T5C4) план уже сейчас",
+            reply_markup=markup,
+            parse_mode="MarkdownV2",
+        )
+    elif query.data == "extended":
+        keyboard = [
+            [
+                InlineKeyboardButton("Получить", callback_data="get"),
+                InlineKeyboardButton("Не интересует", callback_data="no"),
+            ]
+        ]
+        markup = InlineKeyboardMarkup(keyboard)
+        await query.edit_message_text(
+            text="Получите [расширенный](https://parents-chew-q3e.craft.me/S1Jj4dwYTEthRE) план уже сейчас",
+            reply_markup=markup,
+            parse_mode="MarkdownV2",
+        )
+
+
+async def get_lead_answer(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    query.answer()
+    if query.data == "get":
+        await query.edit_message_text(text="Спасибо за ваш выбор")
+    elif query.data == "no":
+        await query.edit_message_text(
+            text="Нам очень жаль. Будем рады видеть вас снова!"
         )
         return FIRST_MESSAGE
 
@@ -172,6 +242,10 @@ if __name__ == "__main__":
                     filters=filters.TEXT & ~filters.COMMAND,
                     callback=get_agreement,
                 )
+            ],
+            INLINE_BUTTON: [
+                CallbackQueryHandler(callback=get_leads, pattern="^(extended|basic)"),
+                CallbackQueryHandler(callback=get_lead_answer, pattern="^(get|no)"),
             ],
         },
         fallbacks=[CommandHandler("start", start)],
